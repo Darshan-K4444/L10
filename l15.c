065d31068369a02557cba99fb6327752d39928f6
Darshan-K4444 <darshan.k@s.amity.edu>
2025-03-05 14:33:41 +0530

    program

---
+++ l15.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+a=10;
+b=15;
+a=++b;
+b=++a;
+printf("%d%d",a,b);
+if(a>b)
+printf("%d",a);
+else
+printf("%d",b);
+if (a==b)
+printf("d",a);
+print("%d%d",a,b);
+return 0;
+}
+
